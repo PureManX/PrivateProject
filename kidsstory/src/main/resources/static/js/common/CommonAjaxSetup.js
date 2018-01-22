96a60055f432d031f6b4693f2e75fa01ce86f100
PureMaN <sakukiller@gmail.com>
2018-01-23 00:51:01 +0900

    ajax 호출 로딩 팝업 처리

--- kidsstory/src/main/resources/static/js/common/CommonAjaxSetup.js
+++ kidsstory/src/main/resources/static/js/common/CommonAjaxSetup.js
@@ -8,6 +8,12 @@ $.ajaxSetup({
 	,
 	error : function(jqXHR, textStatus, errorThrown) {
 		console.log(JSON.stringify(jqXHR));
+		
+		if (jqXHR.readyState != 4) {
+			alert("일시적으로 접속이 원활하지 않습니다.\n잠시 후 다시 이용해\n 주시기 바랍니다.")
+			return false;
+		}
+		
 		if (jqXHR.status == 401) {
 			location.href = "/error/401"
 		} else {
